5781b1615edfb3f337e382beb43417fff277013e
gopher <beatxx@qq.com>
2024-08-08 00:55:24 +0800

    feature(service-system-user): add systemUser service logic

--- app/internal/service/system_user.go
+++ app/internal/service/system_user.go
@@ -2,21 +2,33 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/gopherxin/goadmin/app/internal/biz"
 
 	pb "github.com/gopherxin/goadmin/api/system/user/service/v1"
 )
 
-type SystemUserService struct {
-	pb.UnimplementedSystemUserServer
-}
-
-func NewSystemUserService() *SystemUserService {
-	return &SystemUserService{}
+func NewSystemUserService(service *biz.SystemUserUseCase, logger log.Logger) *SystemUserService {
+	return &SystemUserService{
+		service: service,
+		log:     log.NewHelper(logger),
+	}
 }
 
 func (s *SystemUserService) CreateSystemUser(ctx context.Context, req *pb.CreateSystemUserRequest) (*pb.CreateSystemUserReply, error) {
+	s.log.Infof("input data %v", req)
+	user, err := s.service.CreateUser(ctx, &biz.SystemUser{
+		Mobile:   req.Mobile,
+		Nickname: req.Nickname,
+		Password: req.Password,
+		Username: req.Username,
+	})
+	fmt.Println("user", user)
+	if err != nil {
 
-	return &pb.CreateSystemUserReply{}, nil
+	}
+	return &pb.CreateSystemUserReply{}, err
 }
 func (s *SystemUserService) UpdateSystemUser(ctx context.Context, req *pb.UpdateSystemUserRequest) (*pb.UpdateSystemUserReply, error) {
 	return &pb.UpdateSystemUserReply{}, nil
@@ -28,5 +40,6 @@ func (s *SystemUserService) GetSystemUser(ctx context.Context, req *pb.GetSystem
 	return &pb.GetSystemUserReply{}, nil
 }
 func (s *SystemUserService) ListSystemUser(ctx context.Context, req *pb.ListSystemUserRequest) (*pb.ListSystemUserReply, error) {
+
 	return &pb.ListSystemUserReply{}, nil
 }
