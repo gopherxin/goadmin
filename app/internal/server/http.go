5fe276a762d0341e8730941247160475dbcd8d2f
gopher <beatxx@qq.com>
2024-08-08 00:50:01 +0800

    chore(server): change service

--- app/internal/server/http.go
+++ app/internal/server/http.go
@@ -4,13 +4,14 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/transport/http"
-	v1 "goadmin/api/helloworld/v1"
-	"goadmin/app/internal/conf"
-	"goadmin/app/internal/service"
+	v1 "github.com/gopherxin/goadmin/api/system/user/service/v1"
+
+	"github.com/gopherxin/goadmin/app/internal/conf"
+	"github.com/gopherxin/goadmin/app/internal/service"
 )
 
 // NewHTTPServer new an HTTP server.
-func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.Logger) *http.Server {
+func NewHTTPServer(c *conf.Server, systemUserService *service.SystemUserService, logger log.Logger) *http.Server {
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
@@ -26,6 +27,6 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.L
 		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
 	}
 	srv := http.NewServer(opts...)
-	v1.RegisterGreeterHTTPServer(srv, greeter)
+	v1.RegisterSystemUserHTTPServer(srv, systemUserService)
 	return srv
 }
