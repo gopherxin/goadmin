45e54eaac0ff086cc25d329120cf0288c601b855
gopher <beatxx@qq.com>
2024-08-08 00:49:06 +0800

    feature(data): add gorm

--- app/internal/data/data.go
+++ app/internal/data/data.go
@@ -3,21 +3,53 @@ package data
 import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
-	"goadmin/app/internal/conf"
+	"github.com/gopherxin/goadmin/app/internal/biz"
+	"github.com/gopherxin/goadmin/app/internal/conf"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+
+	// init mysql driver
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // ProviderSet is data providers.
-var ProviderSet = wire.NewSet(NewData, NewGreeterRepo)
+var ProviderSet = wire.NewSet(NewData, NewDB, NewSystemUserRepo)
 
 // Data .
 type Data struct {
-	// TODO wrapped database client
+	db  *gorm.DB
+	log *log.Helper
 }
 
 // NewData .
-func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
+func NewData(db *gorm.DB, logger log.Logger) (*Data, func(), error) {
+	l := log.NewHelper(log.With(logger, "module", "goadmin/base/data"))
 	cleanup := func() {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
-	return &Data{}, cleanup, nil
+	d := &Data{
+		db:  db,
+		log: l,
+	}
+	return d, cleanup, nil
+}
+
+// NewDB gorm Connecting to a Database
+func NewDB(conf *conf.Data, logger log.Logger) *gorm.DB {
+	log := log.NewHelper(log.With(logger, "module", "order-service/data/gorm"))
+
+	db, err := gorm.Open(mysql.Open(conf.Database.Source), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+
+	if err != nil {
+		log.Fatalf("failed opening connection to mysql: %v", err)
+	}
+	if err := db.AutoMigrate(&biz.SystemUser{}); err != nil {
+		log.Fatal(err)
+	}
+	return db
 }
