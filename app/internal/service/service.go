7b48237b72e144a2451334061506d12b9cc0599d
gopher <beatxx@qq.com>
2024-08-08 00:50:37 +0800

    refactor(service): extract systemUserService

--- app/internal/service/service.go
+++ app/internal/service/service.go
@@ -1,6 +1,17 @@
 package service
 
-import "github.com/google/wire"
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+	pb "github.com/gopherxin/goadmin/api/system/user/service/v1"
+	"github.com/gopherxin/goadmin/app/internal/biz"
+)
 
 // ProviderSet is service providers.
 var ProviderSet = wire.NewSet(NewSystemUserService)
+
+type SystemUserService struct {
+	pb.UnimplementedSystemUserServer
+	service *biz.SystemUserUseCase
+	log     *log.Helper
+}
